cf925e3fb8e7b6baaad3df9fde40c9ee00ae2d04
22Santka <22santka@wtahs.com>
2024-04-18 19:44:13 -0400

    Demo-build
    Program barely works and is missing a lot of features, but now works and is functional

--- compression.py
+++ compression.py
@@ -1,30 +1,29 @@
 from tkinter import filedialog
 import os
 import ffmpy
-from ffmpy import FFmpeg
 import filefetch
 
 def compressFile(input_file, output_file):
-    # FFmpeg command for compression, then execute it
-    ff = ffmpy.FFmpeg(inputs = {input_file: None}, outputs = {output_file: f"ffmpeg -i {input_file} -vcodec libx265 -crf 28 {output_file}"})
+    # Setup for the FFmpeg command variable
+    output_path = os.path.abspath(output_file)
+    input_path = os.path.abspath(input_file)
     
-    ff.cmd
+    # FFmpeg command for compression, then execute it. Potential update in the future for easy setting changes
+    ffCmd = f'-y -i "{input_path}" -c:v libx265 -crf 28 -f mp4 "{output_path}"'
+    ff = ffmpy.FFmpeg(inputs = {input_path: None}, outputs = {output_path: ffCmd})
     ff.run()
 
 def optimizeFile():
     if filefetch.filepath: 
         saveFileName = filedialog.asksaveasfilename(title="Save compressed file as", initialfile="compressed_video.mp4", defaultextension=".mp4", filetypes=[("MP4 files", "*.mp4")])
-        
-        # Check if the user selected a save directory and file name, if so, make it. Else, give an error
+    
         if saveFileName:
+            # Check if the user selected a save directory and file name, if so, make it. Else, give an error
             outputFile = saveFileName
             compressFile(filefetch.filepath, outputFile)
             print("Video compression completed successfully!")
-            print(outputFile)
         else:
-            print("Please select a directory and provide a file name for saving the compressed video.")
-    else:
-        print("No video file selected.")
+            print("Please select a location and file name to save the compressed video.")
 
 if __name__ == "__main__":
     optimizeFile()
