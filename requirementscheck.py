08b12e8de76ecb4e2088c430518ca8a5f99034ff
22Santka <22santka@wtahs.com>
2024-04-20 01:02:31 -0400

    Requirements fix
    Requirements check is no longer automatic, but I think it's fixed. Still checking

--- requirementscheck.py
+++ requirementscheck.py
@@ -1,16 +1,4 @@
-import unittest
-from pathlib import Path
-import pkg_resources
-
-_REQUIREMENTS_PATH = Path(__file__).parent.with_name("requirements.txt")
+import os
 
 def doRequirementsTest(self):
-    requirements = pkg_resources.parse_requirements(_REQUIREMENTS_PATH.open())
-    
-    # Use a loop to check and download each requirement in the requirements.txt file, using a subtest for each one independantly
-    for requirement in requirements:
-        requirement = str(requirement)
-        print(requirement)
-        with self.subTest(requirement=requirement):
-            pkg_resources.require(requirement)
-            print(requirement)
+    os.system("pip install -r requirements.txt")
