7c2ba27dd4b2f29bb70b0f443606a26bc97abb4e
22Santka <22santka@wtahs.com>
2024-04-20 00:42:28 -0400

    Update requirementscheck.py

--- requirementscheck.py
+++ requirementscheck.py
@@ -8,7 +8,7 @@ class testRequirements(unittest.TestCase):
     def doRequirementsTest(self):
         requirements = pkg_resources.parse_requirements(_REQUIREMENTS_PATH.open())
         
-        # Use a loop to check and download each requirement in the requirements.txt file, using a subtest
+        # Use a loop to check and download each requirement in the requirements.txt file, using a subtest for each one independantly
         for requirement in requirements:
             requirement = str(requirement)
             with self.subTest(requirement=requirement):
