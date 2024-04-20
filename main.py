4b96dd1af8ed9940c1962442767a46c10c541f67
22Santka <22santka@wtahs.com>
2024-04-20 00:45:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from maintkinterwindow import MainTkinterWindowSetup # Imported class from another file
 from requirementscheck import testRequirements # Imported class from another file
-import os
         
 def main():
+    from maintkinterwindow import MainTkinterWindowSetup # Imported class from another file
     MainTkinterWindowSetup.mainWindow(MainTkinterWindowSetup)
 
 if __name__ == "__main__":
