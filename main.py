e5968ff1d4eb84588bd6696266aefb8643466a6a
22Santka <22santka@wtahs.com>
2024-04-20 00:50:56 -0400

    Requirements fix?
    Attempt to fix things. Requirements check broke

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from requirementscheck import testRequirements # Imported class from another file
+from maintkinterwindow import MainTkinterWindowSetup # Imported class from another file
         
 def main():
-    from maintkinterwindow import MainTkinterWindowSetup # Imported class from another file
     MainTkinterWindowSetup.mainWindow(MainTkinterWindowSetup)
 
 if __name__ == "__main__":
-    testRequirements.doRequirementsTest(testRequirements())
     main()
--- requirementscheck.py
+++ requirementscheck.py
@@ -4,12 +4,13 @@ import pkg_resources
 
 _REQUIREMENTS_PATH = Path(__file__).parent.with_name("requirements.txt")
 
-class testRequirements(unittest.TestCase):
-    def doRequirementsTest(self):
-        requirements = pkg_resources.parse_requirements(_REQUIREMENTS_PATH.open())
-        
-        # Use a loop to check and download each requirement in the requirements.txt file, using a subtest for each one independantly
-        for requirement in requirements:
-            requirement = str(requirement)
-            with self.subTest(requirement=requirement):
-                pkg_resources.require(requirement)
+def doRequirementsTest(self):
+    requirements = pkg_resources.parse_requirements(_REQUIREMENTS_PATH.open())
+    
+    # Use a loop to check and download each requirement in the requirements.txt file, using a subtest for each one independantly
+    for requirement in requirements:
+        requirement = str(requirement)
+        print(requirement)
+        with self.subTest(requirement=requirement):
+            pkg_resources.require(requirement)
+            print(requirement)
